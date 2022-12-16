e450d90696aa4fb30185b9462657c4234efa1a03
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-12-16 20:05:36 -0300

    dandole estilo a la parte al componente con los puntos del user

--- src/component/private/UserInfoHeader.jsx
+++ src/component/private/UserInfoHeader.jsx
@@ -1,12 +1,13 @@
 const UserInfoHeader=({data})=>{
   return(
-    <div>
+    <div className="flex text-white">
       <div>
         <p>Mis puntos</p>
       </div>
-      <div>
-        {data}
-        <img src='./assets/icons/menu/icon_avatar_white.svg' alt="" />
+      <div className="flex mx-4">
+        
+        <p>{data}</p>
+        <img src='./assets/icons/menu/icon_avatar_white.svg' className=" w-16 h-16" alt="" />
       </div>
     </div>
   )
