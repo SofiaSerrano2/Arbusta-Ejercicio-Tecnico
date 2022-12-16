35da3239b12a0289c792c31d3a4ccbd37c67df4c
sofiaserrano2 <sofia.serrano@arbusta.net>
2022-12-16 08:20:21 -0300

    se crea validaciones y campos del form

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -2,23 +2,48 @@ import { useEffect } from "react";
 import { useDispatch ,useSelector} from 'react-redux'
 import {editUserName} from '../reducer/userReducer'
 import serviceUserDetail from '../service/userDetail'
-
+import { useFormik } from 'formik';
+import { getValidationSchema } from '../validationSchema/getValidationSchema';
+import TextInput from './Inputs/TextInput'
 //viendo si me conviene hacerlo Componente o ponerlo directamente en la página
 const  Form=()=> {
   const dispatch = useDispatch()
   const user=useSelector(state => state.user)
+
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    errors,
+    values,
+    touched
+  } = useFormik({
+    initialValues: {
+      name: '',
+    },
+    validationSchema: getValidationSchema()
+  })
 
   const editName = (event) => {
     event.preventDefault();
     const content = event.target.name.value;
     dispatch(editUserName(content));
   }
-  
+
   return ( 
       <>
         <h1>Datos Personales</h1>
         <form action=""  onSubmit={editName}>
-            <input name="name" type='text'/>
+        <TextInput
+          label='Nombre'
+          name='name'
+          placeholder={user.name}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          touched={touched.name}
+          values={values.name}
+          error={errors.name}
+          />
             <button type="submit">add</button>
           </form>
         </>
--- src/validationSchema/getValidationSchema.jsx
+++ src/validationSchema/getValidationSchema.jsx
@@ -6,6 +6,13 @@ import * as Yup from 'yup';
 //dni
 //tenes licencia de conducir
 export const getValidationSchema = () => Yup.object().shape({
-   
-
+    name: Yup.string('Campo inválido')
+    .min(2,'Entre 2 y 500 caracteres')
+    .max(500,'')
+    .required('Campo requerido'),
+    surname: Yup.string('Campo inválido')
+    .min(2,'Entre 2 y 500 caracteres')
+    .max(500,'')
+    .required('Campo requerido'),
+    email:
 })
